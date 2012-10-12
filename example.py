6766e398b28bd9381cd8af383e2195b30cf616b9
yattom <tsutomu.yasui@gmail.com>
2012-10-12 19:06:19 +0900

    complete example of Game of Life

--- example.py
+++ example.py
@@ -17,6 +17,12 @@ class Cell(object):
     def tick(self):
         self.state = self.next_state
 
+    def live(self):
+        self.state = Cell.ALIVE
+
+    def die(self):
+        self.state = Cell.DEAD
+
 class LifeGame(object):
     NEIGHBORS = [
         (-1, -1), ( 0, -1), (1, -1),
@@ -62,6 +68,69 @@ class LifeGame(object):
                 s += u'*' if self.cells[(x, y)].state else u'_'
             s += '\n'
         return s
+
+    def reset(self, cells=[]):
+        for y, l in enumerate(cells):
+            for x, c in enumerate(l):
+                switch(c,
+                       '_', lambda: self.cells[(x, y)].die(),
+                       '*', lambda: self.cells[(x, y)].live(),
+                       '*', lambda: self.cells.__setitem__((x, y), Cell.ALIVE))
+
+
+class LifeGameFn(object):
+    NEIGHBORS = [
+        (-1, -1), ( 0, -1), (1, -1),
+        (-1,  0),           (1,  0),
+        (-1,  1), ( 0,  1), (1,  1),
+    ]
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.create_cells()
+        self.bind_cells()
+
+    def create_cells(self):
+        self.cells = {}
+        for x in range(self.w):
+            for y in range(self.h):
+                self.cells[(x, y)] = Cell()
+
+    def bind_cells(self):
+        for x, y in self.cells:
+            cell = self.cells[(x, y)]
+            for dx, dy in LifeGame.NEIGHBORS:
+                n = self.cells.get((x + dx, y + dy), None)
+                if n:
+                    cell.neighbors.append(n)
+
+    def next(self):
+        for cell in self.cells.values():
+            count = len(filter(lambda c: c.state == Cell.ALIVE, cell.neighbors))
+            cell.next_state = switch(
+                count,
+                2, cell.state,
+                3, Cell.ALIVE,
+                Cell.DEAD)
+        for cell in self.cells.values():
+            cell.tick()
+
+
+    def show(self):
+        s = ''
+        for y in range(self.h):
+            for x in range(self.w):
+                s += u'*' if self.cells[(x, y)].state else u'_'
+            s += '\n'
+        return s
+
+    def reset(self, cells=[]):
+        for y, l in enumerate(cells):
+            for x, c in enumerate(l):
+                switch(c,
+                       '_', lambda: self.cells[(x, y)].die(),
+                       '*', lambda: self.cells[(x, y)].live(),
+                       '*', lambda: self.cells.__setitem__((x, y), Cell.ALIVE))
 
 
 def main():
--- example_test.py
+++ example_test.py
@@ -2,11 +2,44 @@
 
 import unittest
 
-from example import LifeGame
+from example import LifeGameFn, LifeGame
 
 class LifeGameTest(unittest.TestCase):
     def test_overall(self):
         game = LifeGame(10, 10)
+        s = ['__*_______',
+             '*_*____***',
+             '_**_______',
+             '__________',
+             '__________',
+             '__________',
+             '__________',
+             '_______*__',
+             '**____*_*_',
+             '**___*___*']
+        game.reset(s)
+
+        for i in range(10):
+            game.next()
+
+        actual = game.show()
+        expected = ['__________',
+                    '_______***',
+                    '____*_____',
+                    '_____*____',
+                    '___***____',
+                    '__________',
+                    '_______*__',
+                    '______*_*_',
+                    '**____*_*_',
+                    '**_____*__']
+
+        self.assertEqual(actual, '\n'.join(expected) + '\n')
+
+
+class LifeGameFnTest(unittest.TestCase):
+    def test_overall(self):
+        game = LifeGameFn(10, 10)
         s = ['__*_______',
              '*_*____***',
              '_**_______',
