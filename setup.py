ceb1ae67d4743303f1baa2c0260e214187788ced
yattom <tsutomu.yasui@gmail.com>
2012-11-26 13:27:53 +0900

    installable now!

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "pypattern",
+    version = "0.0.1",
+    author = "yattom",
+    author_email = "tsutomu.yasui@gmail.com",
+    packages = ['pattern', 'examples']
+)
